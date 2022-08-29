f4991f99e2c76eae981e876cb7be561a2ff87640
L-Grim <111025216+L-Grim@users.noreply.github.com>
2022-08-29 19:51:56 -0300

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+char mystring[51]; 
+
+printf("Digite seu nome: ");
+scanf("%s" , mystring);
+
+printf("O nome armazenado em mystring e = %s" ,mystring);
+
+return 0;
+}
