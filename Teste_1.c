ad6f23b7a2b02e22c34c28600b73fde5079ebd97
L-Grim <111025216+L-Grim@users.noreply.github.com>
2022-08-31 19:55:26 -0300

    Add files via upload

---
+++ Teste_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void); {
+    // Entrada
+    int a = 10;
+    int b = 20;
+
+    // Processamento
+    if (a > b) {
+        // Saida
+        printf("a = %d > b = %d" a, b);
+    }
+    else (a < b) {
+        // Saida
+         printf("a = %d <= b = %d\n", a, b);
+    }
+
+
+    return 0;
+
+}
